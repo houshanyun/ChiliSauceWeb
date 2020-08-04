edb957e98caeae437759369c1e80c8dc6cfe5d85
houshanyun <likeredbean@gmail.com>
2020-08-04 08:26:39 +0800

    chili_0.3

--- chiliweb/views.py
+++ chiliweb/views.py
@@ -5,3 +5,20 @@ from chiliweb.models import Buylist
 @app.route('/', methods=['GET'])
 def index():
     return render_template('index.html')
+
+@app.route('/buy', methods=['GET', 'POST'])
+def buy():
+    if request.method == 'POST':        
+        name = request.form['name']
+        address = request.form['address']
+        phone = request.form['phone']
+        email = request.form['email']
+        quantity = request.form['quantity']
+
+        buylist = Buylist(name=name, address=address, phone=phone, email=eamail, quantity=quantity)
+        db,sessiona.add(buylist)
+        db.session.commit()
+        flash('已送出訂單!')
+        return redirect(url_for('index'))
+
+    return render_template('buy.html')
