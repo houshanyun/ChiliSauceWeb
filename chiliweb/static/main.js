0a514240a9011b187d261e1f9f1cd8a2f0860cd9
houshanyun <likeredbean@gmail.com>
2020-08-07 08:18:45 +0800

    'chili_0.41'

--- chiliweb/static/main.js
+++ chiliweb/static/main.js
@@ -1,7 +1,10 @@
 function formFuc() {
-    if (confirm('總共XXX元，確定要訂購嗎?')) {
+    let count = document.getElementById('quantity');
+    let c = count.value;
+    let tol = c*300;
+    if (confirm('總共' + tol + '元，確定要訂購嗎?')) {
         alert('已送出訂單...')
     } else {
         alert('訂單已取消...')
     }
-}
+};
