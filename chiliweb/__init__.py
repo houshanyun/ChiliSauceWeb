c45fb1ff4f22c898e6b61e3af7000d473e682dce
houshanyun <likeredbean@gmail.com>
2020-08-09 13:30:51 +0800

    'sql'

--- chiliweb/__init__.py
+++ chiliweb/__init__.py
@@ -14,8 +14,7 @@ app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'gsfi34789hufds')
 #app.config['SQLALCHEMY_DATABASE_URI'] = f"mysql+pymysql://{os.getenv('USER')}:{os.getenv('PASSWORD')}@127.0.0.1:3306/data"
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mxstiuaxibfxyh:f5570063166016f79dc1d38fb21897910d757892acad72506448d6a744db5d51@ec2-35-175-155-248.compute-1.amazonaws.com:5432/d2q1biiblsoihg'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(os.path.dirname(app.root_path), os.getenv('DATABASE_FILE', 'data.db'))
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
